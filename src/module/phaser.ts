a3342630b90da6ccd676acc9c761dcb72759cf1f
JxxH204 <57583918+jxxh204@users.noreply.github.com>
2022-11-28 15:31:19 +0900

    v0.0.0:10 FEAT : player 하나로 합침

--- src/module/phaser.ts
+++ src/module/phaser.ts
@@ -17,23 +17,23 @@ export class Sprites extends Phaser.Scene
 {
    bg:object
    player:Sprites
+   playerState:string
    jump_player:Sprites
    walk_player:Sprites
    platforms:any
    cursors:any;
    playerLocation:object
-   imageSet:string
 
     constructor ()
     {
         super();
         this.bg = {}
         this.player = {} as Sprites
+        this.playerState = "_idle"
         this.jump_player = {} as Sprites
         this.walk_player = {} as Sprites
         this.platforms = {}
         this.cursors = {}
-        this.imageSet = ""
         this.playerLocation = {
           w:window.innerWidth/2,
           h:window.innerHeight
@@ -44,9 +44,9 @@ export class Sprites extends Phaser.Scene
     {
       this.load.image('background', background);
       // this.load.image('player1', Mushrooms.idle1);
-      this.load.spritesheet('idle_player1', Mushrooms["idle"],{ frameWidth: 32, frameHeight: 28 })
-      this.load.spritesheet('jump_player1', Mushrooms["jump"],{ frameWidth: 32, frameHeight: 32 })
-      this.load.spritesheet('walk_player1', Mushrooms["walk"],{ frameWidth: 32, frameHeight: 28 })
+      this.load.spritesheet('player_idle', Mushrooms["idle"],{ frameWidth: 32, frameHeight: 28 })
+      this.load.spritesheet('player_jump', Mushrooms["jump"],{ frameWidth: 32, frameHeight: 32 })
+      this.load.spritesheet('player_walk', Mushrooms["walk"],{ frameWidth: 32, frameHeight: 28 })
     }
 
     create ()
@@ -54,48 +54,38 @@ export class Sprites extends Phaser.Scene
       this.bg = this.add.image(400, 300, 'background');
       this.platforms = this.physics.add.staticGroup();
 
-      this.player = this.physics.add.sprite(this.playerLocation.w, this.playerLocation.h, 'idle_player1');
-      this.jump_player = this.physics.add.sprite(this.playerLocation.w+50, this.playerLocation.h,"jump_player1")
-      this.walk_player = this.physics.add.sprite(this.playerLocation.w-50, this.playerLocation.h,"walk_player1")
-
+      this.player = this.physics.add.sprite(this.playerLocation.w, this.playerLocation.h, `player${this.playerState}`);
       this.player.setBounce(0.2);
       this.player.setCollideWorldBounds(true);
 
-      this.jump_player.setBounce(0.2);
-      this.jump_player.setCollideWorldBounds(true);
-
-      this.walk_player.setBounce(0.2);
-      this.walk_player.setCollideWorldBounds(true);
-
-      // 
       this.anims.create( {
           key: 'idle',
-          frames: this.anims.generateFrameNames('idle_player1',{start:0, end:7}),
+          frames: this.anims.generateFrameNames('player_idle',{start:0, end:1}),
           frameRate: 4,
           repeat: -1
       });
       this.anims.create({
         key: 'jump',
-        frames: this.anims.generateFrameNames('jump_player1',{start:0, end:7}),
+        frames: this.anims.generateFrameNames('player_jump',{start:0, end:7}),
         frameRate: 16,
         repeat: 0
     });
     this.anims.create({
       key: 'left',
-      frames: this.anims.generateFrameNames('walk_player1',{start:0, end:3}),
+      frames: this.anims.generateFrameNames('player_walk',{start:0, end:3}),
       frameRate: 8,
       repeat: 0
   });
   this.anims.create({
     key: 'right',
-    frames: this.anims.generateFrameNames('walk_player1',{start:0, end:3}),
+    frames: this.anims.generateFrameNames('player_walk',{start:0, end:3}),
     frameRate: 8,
     repeat: 0
 });
 
 
 
-      this.player.play("idle") // idle 모션 실행.
+      this.player.play("idle",true) // idle 모션 실행.
 
         this.cursors = this.input.keyboard.createCursorKeys(); // 키보드 사용
 
@@ -105,23 +95,30 @@ export class Sprites extends Phaser.Scene
     {
 
       if (this.cursors.left.isDown) {
-        this.walk_player.setVelocityX(-40);
-        this.walk_player.anims.play('left', true);
+        this.player.setVelocityX(-40);
+        this.player.anims.play('left',true);
       } else if (this.cursors.right.isDown) {
-          this.walk_player.setVelocityX(40);
-          this.walk_player.anims.play('right', true);
+          this.player.setVelocityX(40);
+          this.player.anims.play('right', true);
       } else if(this.cursors.space.isDown) {
-        this.jump_player.setVelocityY(300);
-      this.jump_player.play("jump")
+        console.log(this.playerState )
+        this.playerState = "_jump"
+        this.player.setVelocityY(300);
+        this.player.play("jump")
 
       } else {
-        console.log("멈춤")
-        this.walk_player.setVelocityX(0);
-        // this.walk_player.anims.play('left', true);
-      }
-        if (this.cursors.left.isUp || this.cursors.right.isUp)
-      {
+        if(this.playerState === "_jump") { //점프를 누른 후
+          setTimeout(() => {
+            this.playerState = "_idle"
+            this.player.play("idle",true)
+          },500)
+        } else {
           this.player.setVelocityX(0);
+          this.playerState = "_idle"
+          this.player.play("idle",true)
+        }
+
+        // this.walk_player.anims.play('left', true);
       }
 
       // if (this.cursors.left.isDown)
