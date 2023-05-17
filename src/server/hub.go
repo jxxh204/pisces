6a0c6e3875f0d4ba5445ac7419d1028ae703848c
JaeHwan <ahhancom@gmail.com>
2023-05-17 21:51:17 +0900

    v0.0.0:97 - 브로드 캐스트 시 id를 검사하여 나말고 다른 유저에게만 메세지를 전달. - clientID를 서버에서 작성

--- src/server/hub.go
+++ src/server/hub.go
@@ -6,23 +6,22 @@ package main
 
 // Hub maintains the set of active clients and broadcasts messages to the
 // clients.
+type BroadcastMessage struct {
+    UserID string
+    Data   []byte
+}
 type Hub struct {
 	// Registered clients.
 	clients map[*Client]bool
-
-	// Inbound messages from the clients.
-	broadcast chan []byte
-
-	// Register requests from the clients.
+	broadcast chan *BroadcastMessage
 	register chan *Client
-
-	// Unregister requests from clients.
 	unregister chan *Client
 }
 
+
 func newHub() *Hub {
 	return &Hub{
-		broadcast:  make(chan []byte),
+		broadcast:  make(chan *BroadcastMessage),
 		register:   make(chan *Client),
 		unregister: make(chan *Client),
 		clients:    make(map[*Client]bool),
@@ -41,13 +40,16 @@ func (h *Hub) run() {
 			}
 		case message := <-h.broadcast:
 			for client := range h.clients {
-				select {
-				case client.send <- message:
-				default:
-					close(client.send)
-					delete(h.clients, client)
-				}
+				//여기서 다 걸러낼 수 있음.
+				if client.userID != message.UserID { // 특정 유저에게 전달하기 위해 추가.
+					select {
+					case client.send <- message.Data:
+					default:
+						close(client.send)
+						delete(h.clients, client)
+					}
 			}
 		}
 	}
+}
 }
